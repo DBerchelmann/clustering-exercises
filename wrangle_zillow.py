35bb7dbb596667ae3426b08ff042f22b53345480
David Berchelmann <david.a.berchelmann@gmail.com>
2021-03-31 22:25:37 -0500

    upload of files

--- wrangle_zillow.py
+++ wrangle_zillow.py
@@ -96,6 +96,118 @@ def clean_zillow(df):
     
     
     
+    # Create new column (age_of_home)
+    today = pd.to_datetime('today')
+    df['age_of_home'] = today.year - df['yearbuilt']
+    
+    # clean up longitude (this cleans latitude as well)
+    longindex = df.loc[df['longitude'].isin(['NaN'])].index
+    df.drop(longindex , inplace=True)
+        
+    # drop rows that have a property id less than 250 (not single unit)
+    cleanpropindexes250 = df.loc[df['propertylandusetypeid'] <= 250].index
+    df.drop(cleanpropindexes250 , inplace=True)
+    
+    # drop any rows that have unit count greater than one (39 rows)
+    singlecountindexes = df.loc[df['unitcnt'] > 1].index
+    df.drop(singlecountindexes , inplace=True)
+    
+    # turn null values in pool into value 0
+    df['poolcnt'].fillna(0, inplace=True)
+    
+    # Create bathrooms per sqft
+    df['bath_pers_qft'] = df['bathroomcnt'] / df['calculatedfinishedsquarefeet']
+    
+    indextaxvalue = df.loc[df['taxvaluedollarcnt'].isin(['NaN'])].index
+    df.drop(indextaxvalue , inplace=True)
+    
+    # If "fireplaceflag" is "True" and "fireplacecnt" is "NaN", we will set "fireplacecnt" equal to the median value of "1".
+    df.loc[(df['fireplaceflag'] == True) & (df['fireplacecnt'].isnull()), ['fireplacecnt']] = 1
+    
+    # If 'fireplacecnt' is "NaN", replace with "0"
+    df.fireplacecnt.fillna(0,inplace = True)
+    
+    # If "fireplacecnt" is 1 or larger "fireplaceflag" is "NaN", we will set "fireplaceflag" to "True".
+    df.loc[(df['fireplacecnt'] >= 1.0) & (df['fireplaceflag'].isnull()), ['fireplaceflag']] = True
+    df.fireplaceflag.fillna(0,inplace = True)
+    
+    # Convert "True" to 1
+    df.fireplaceflag.replace(to_replace = True, value = 1,inplace = True)
+    
+    # Remove NaNs from tax amount
+    indextax = df.loc[df['taxamount'].isin(['NaN'])].index
+    df.drop(indextax , inplace=True)
+    
+    # Remove decimal from latitude and longitude
+    df['latitude'] = df['latitude'].astype(int)
+    df['longitude'] = df['longitude'].astype(int)
+    
+    # Convert latitude and longitude to positonal data points using lambda funtion (i.e. putting a decimal in the correct place)
+    df['latitude'] = df['latitude'].apply(lambda x: x / 10 ** (len((str(x))) - 2))
+    df['longitude'] = df['longitude'].apply(lambda x: x / 10 ** (len((str(x))) - 4))
+    
+    
+    # Remove null years (small amount)
+    indexyear = df.loc[df['yearbuilt'].isin(['NaN'])].index
+    df.drop(indexyear , inplace=True)
+    
+    # change columns to int
+    df['fips'] = df['fips'].astype(int)
+    df['yearbuilt'] = df['yearbuilt'].astype(int)
+    
+    # Fill in lot size square footage nulls with median
+    df['lotsizesquarefeet'].fillna((df['lotsizesquarefeet'].median()), inplace=True)
+    
+    # Clean up square footage
+    indexsquarefeet = df.loc[df['calculatedfinishedsquarefeet'].isnull()].index 
+    df.drop(indexsquarefeet, inplace=True)
+    
+    # Clean up tax amount
+    indextaxamount = df.loc[df['taxamount'].isnull()].index 
+    df.drop(indextaxamount, inplace=True)
+
+    # Fill Bedroom Nulls
+    df['bedroomcnt'].replace(0,df['bedroomcnt'].median(axis=0),inplace=True)
+    
+    # Fill Bathroom Nulls
+    df['bathroomcnt'].replace(0,df['bathroomcnt'].median(axis=0),inplace=True)
+    
+    # Heating System
+    df["heatingorsystemdesc"].fillna("None", inplace = True)
+    df['heatingorsystemtypeid'].fillna(1, inplace=True)
+    
+    # AC
+    df["airconditioningdesc"].fillna("None", inplace = True)
+    df["airconditioningtypeid"].fillna(5,inplace = True)
+
+    # Drop columns
+    dropcols = ['regionidzip', 'finishedsquarefeet12', 'propertyzoningdesc', 'buildingqualitytypeid', 'regionidzip', 'calculatedbathnbr', 
+                'fullbathcnt', 'landtaxvaluedollarcnt', 'structuretaxvaluedollarcnt', 'censustractandblock', 'regionidcity', 'unitcnt',
+                'rawcensustractandblock', 'Unnamed: 0', 'propertycountylandusecode', 'regionidcounty', 'assessmentyear', 'propertylandusetypeid', 'id']
+    df.drop(dropcols, axis=1, inplace=True)
+    
+    # convert columns to object
+    df['fips'] = df['fips'].astype(object)
+    df['yearbuilt'] = df['yearbuilt'].astype(object)
+    
+     # Columns to look for outliers
+    df = df[df.taxvaluedollarcnt < 3_000_000]
+    df = df[df.calculatedfinishedsquarefeet < 8000]
+    
+    dummy_df = pd.get_dummies(df['fips'])
+    dummy_df.columns = ['la_county', 'orange_county', 'ventura_county']
+    df = pd.concat([df, dummy_df], axis=1)
+    
+   
+      
+    # convert column to date time
+    df['transactiondate'] = pd.to_datetime(df['transactiondate'])
+    
+    # Set parcelid as the index
+    df = df.set_index('parcelid')
+    
+
+ 
     
     return df
 
@@ -129,6 +241,7 @@ def missing_zero_values_table(df):
         and also the data types. The message prints out the shape of the data frame and also tells you how many columns have nulls '''
     
     
+    
     zero_val = (df == 0.00).astype(int).sum(axis=0)
     null_count = df.isnull().sum()
     mis_val_percent = 100 * df.isnull().sum() / len(df)
@@ -148,7 +261,7 @@ def missing_zero_values_table(df):
 
     return mz_table
 
-missing_zero_values_table(df)
+
 
 
 def handle_missing_values(df, prop_required_row = 0.5, prop_required_col = 0.5):
@@ -191,3 +304,14 @@ def features_missing(df):
     df2 = df2[['total_features_missing', 'pct_features_missing', 'total_rows_affected']]
     
     return df2
+
+def outlier_function(df, cols, k):
+	#function to detect and handle oulier using IQR rule
+    for col in df[cols]:
+        q1 = df.annual_income.quantile(0.25)
+        q3 = df.annual_income.quantile(0.75)
+        iqr = q3 - q1
+        upper_bound =  q3 + k * iqr
+        lower_bound =  q1 - k * iqr     
+        df = df[(df[col] < upper_bound) & (df[col] > lower_bound)]
+    return df
